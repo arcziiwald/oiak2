4d9f3e38b37e74a87f198f7f88ccd5051c5e2dc0
Artur Ciszak <artur.ciszak@morefromit.com>
2019-05-06 19:50:07 +0200

    Podłączenie przycisku "start"

--- app/src/main/java/example/com/oiak/MainActivity.java
+++ app/src/main/java/example/com/oiak/MainActivity.java
@@ -3,13 +3,23 @@ package example.com.oiak;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnStart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnStart = (Button) findViewById(R.id.button);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startGeneratingData(v);
+            }
+        });
     }
 
     public void startGeneratingData(View view) {
@@ -18,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
          * computing*/
         BubbleSort bs = new BubbleSort();
         bs.sortSilly();
-
         bs.sortWisely();
 
     }
